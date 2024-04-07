5c81ee7835e35c89166ca8a55b26445bfa8c139f
Bryan Hernandez <bhdzit97@gmail.com>
2024-04-07 13:45:57 -0600

    Correcion en parametro torre

--- routes/cliente.js
+++ routes/cliente.js
@@ -10,7 +10,6 @@ router.get('/getClientesSuspendidos', getClientesSuspendidos);
 router.post('/saveCliente', [
     body("cliente", "El Cliente es requerido").notEmpty(),
     body('contrato', 'El Contrato es obligatorio').isBoolean().notEmpty(), 
-    body('torre', 'La Torres es obligatorio').isString().notEmpty(),
     body('paquete', 'El Paquete es obligatorio').isString().notEmpty(),
     body('primer_pago', 'El Primer Pago es obligatorio').isString().notEmpty(),
     body('lat', 'La Ubiacion  es obligatoria').isString().notEmpty(),
